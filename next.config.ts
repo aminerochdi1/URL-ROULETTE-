779030d0180e1b7c285e6eec491c0535169d365b
Mohammed Amine Rochdi <mo.aminerochdi@gmail.com>
2025-07-11 18:52:31 +0200

    Configuration next.config.ts for Static Export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // To Generates static HTML/CSS/JS
+  distDir: 'dist',  // Output folder (loaded in Chrome as unpacked extension)
+  // To Add images.unoptimized: true if using Next.js Image
+  images: {
+    unoptimized: true, // Disables Image Optimization API 
+  },
+  // Enable TypeScript for ESLint 
+  eslint: {
+    dirs: ['src'],
+  },
+  // Enable source maps for debugging
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
